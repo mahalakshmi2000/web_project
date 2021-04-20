c8154c952e4707a9820183567909edb7bc4dae0d
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2021-04-20 21:29:37 +0530

    added calculator files

---
+++ Calculator/calculator.js
@@ -0,0 +1,44 @@
+var operator;
+var first_number;
+var second_number
+function Calculator(num){
+	//console.log(num.value);
+	if(num.value == "C"){
+		document.getElementById("display").value = "";
+	}
+	else {
+		if(num.value == "+" || num.value == "-" || num.value == "*" || num.value == "/" || num.value == "%" || num.value == "+/-" ){
+		first_number = +document.getElementById("display").value;
+		operator = num.value;
+		document.getElementById("display").value = "";
+		
+	}
+		else if(num.value == "="){
+			second_number = +document.getElementById("display").value;
+			if(operator == "+"){
+				
+				document.getElementById("display").value = first_number + second_number;
+			}
+			else if(operator == "-"){
+				document.getElementById("display").value = first_number - second_number;
+			}
+			else if(operator == "*"){
+				document.getElementById("display").value = first_number * second_number;
+			}
+			else if(operator == "/"){
+				document.getElementById("display").value = first_number / second_number;
+			}
+			else if(operator == "%"){
+				document.getElementById("display").value = first_number % second_number;
+			}
+			else if(operator == "+/-"){
+				document.getElementById("display").value = first_number + second_number+",";
+				document.getElementById("display").value += first_number - second_number;
+			}
+		}
+		else{
+		document.getElementById("display").value  += num.value;
+	}
+}
+}
+
